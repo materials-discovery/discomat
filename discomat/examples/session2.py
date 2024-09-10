97ec15e51791f9e8e29d482c9e6d1b2d3ee83cd3
Adham Hashibon <78446902+adhamhashibon@users.noreply.github.com>
2024-09-10 15:56:02 +0100

    example updates

--- discomat/examples/session2.py
+++ discomat/examples/session2.py
@@ -22,35 +22,5 @@ gvis(g, "g.html")
 
 
 s = Session()
-s.add(sim)
+s.add_cuds(sim)
 
-# graph into cuds, given a subject, the function searches the graph for label, iri, description, etc and then calls
-# Cuds. and then adds the graph to teh _graph of the Cuds, however, we need to check that it contains only first
-# order relations! or do we relax this and allow a cuds to be anything?
-
-# we need a tighter control over Cuds.attributes and Cuds._graph. I think I must go back any make sure
-"""
-1. we can do a search over a graph, and only retain those relations that are first order neighbors to one particular 
-subject. 
-2. if iri. description, subject, uuid, etc are not in the graph, create them (using __init__). 
-
-we cannot have cuds.attribute which is not first in the graph, and doing cuds.uuid=something should be an interface 
-to add it to the cuds_graph. 
-
-rename _graph to cuds_graph. 
-
-cuds are atomic then, in the sense they describe one subject. 
-
-the subject could have graphs, essentially this is session.
-
-
-
-Option 2: 
-
-Cuds are not atomic, but can be made so. Hence they can be anything. 
-
-atomic_cuds = To_atom(graph/Cuds). 
-
-what are the interfaces of rdflib graphs? 
-
-"""
--- discomat/examples/words.py
+++ discomat/examples/words.py
@@ -12,6 +12,7 @@ from mnemonic import Mnemonic
 mnemo = Mnemonic("english")
 
 words = mnemo.generate(strength=128)
+print(words)
 print ('_'.join(random.sample(list(words.split()), 3)))
 seed = mnemo.to_seed(words, passphrase="")
 entropy = mnemo.to_entropy(words)
