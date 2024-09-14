5d3e94589b996c6ad031e7e5fec0d3419a66ac01
Owain Beynon <54601295+owainbeynon@users.noreply.github.com>
2024-09-14 10:24:21 +0100

    add requirememnts

--- setup.py
+++ setup.py
@@ -5,6 +5,9 @@ setup(
     version="0.01",
     packages=find_packages(),
     install_requires=[
-        # List your dependencies here if any
+        'pyvis',
+        'mnemonic',
+        'rdlib',
+        'uuid'
     ],
 )
